400d4f980f70c1e292a2b6dbf87d850bf759c7ff
choijihyeon <gus1043@gmail.com>
2023-11-13 22:46:36 +0900

    feat : main fragment

--- app/src/main/java/com/example/languageribbon_front/MainFragment.kt
+++ app/src/main/java/com/example/languageribbon_front/MainFragment.kt
@@ -1,17 +1,153 @@
 package com.example.languageribbon_front
 
+import android.content.pm.PackageManager
+import android.media.MediaRecorder
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.fragment.app.Fragment
+import com.example.languageribbon_front.databinding.FragmentMainBinding
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    private var mediaRecorder: MediaRecorder? = null
+    private var audioFilePath: String? = null
+    private var isRecording = false
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button.setOnClickListener {
+            if (isRecording) {
+                stopRecording()
+            } else {
+                startRecording()
+            }
+            updateButtonImage()
+        }
+    }
+    private fun updateButtonImage() {
+        val imageResource = if (isRecording) {
+            R.drawable.playingbtn
+        } else {
+            R.drawable.playbtn
+        }
+        binding.button.setImageResource(imageResource)
+    }
+
+    private fun startRecording() {
+        if (checkPermissions()) {
+            val fileName = "audio_${SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())}.mp3"
+            audioFilePath = "${requireContext().externalCacheDir?.absolutePath}/$fileName"
+
+            mediaRecorder = MediaRecorder().apply {
+                setAudioSource(MediaRecorder.AudioSource.MIC)
+                setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+                setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+                setOutputFile(audioFilePath)
+                try {
+                    prepare()
+                    start()
+                    isRecording = true
+                    Toast.makeText(requireContext(), "Recording started", Toast.LENGTH_SHORT).show()
+
+                    // Update the button image when recording starts
+                    binding.button.setBackgroundResource(R.drawable.playingbtn)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+
+    private fun stopRecording() {
+        mediaRecorder?.apply {
+            stop()
+            reset()
+            release()
+            isRecording = false
+            Toast.makeText(requireContext(), "Recording stopped", Toast.LENGTH_SHORT).show()
+
+            // Update the button image when recording stops
+            binding.button.setBackgroundResource(R.drawable.playbtn)
+
+            sendAudioToBackend()
+        }
+    }
+
+
+    private fun sendAudioToBackend() {
+        audioFilePath?.let { filePath ->
+            val audioFile = File(filePath)
+            if (audioFile.exists()) {
+                val audioRequestBody = audioFile.asRequestBody("audio/*".toMediaTypeOrNull())
+                val audioPart = MultipartBody.Part.createFormData("audio", audioFile.name, audioRequestBody)
+
+//                // Replace the following with your actual API call
+//                val apiService = RetrofitClient.createService(ApiService::class.java)
+//                val call = apiService.uploadAudioFile(audioPart)
+//                call.enqueue(object : Callback<YourResponseClass> {
+//                    override fun onResponse(call: Call<YourResponseClass>, response: Response<YourResponseClass>) {
+//                        // Handle success
+//                        Toast.makeText(requireContext(), "Audio sent to backend", Toast.LENGTH_SHORT).show()
+//                    }
+//
+//                    override fun onFailure(call: Call<YourResponseClass>, t: Throwable) {
+//                        // Handle failure
+//                        Toast.makeText(requireContext(), "Failed to send audio", Toast.LENGTH_SHORT).show()
+//                    }
+//                })
+            }
+        }
+    }
+
+    private fun checkPermissions(): Boolean {
+        return (checkSelfPermission(requireContext(), android.Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
+                && checkSelfPermission(requireContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+    }
+
+    private fun requestPermissions() {
+        val permissions = arrayOf(
+            android.Manifest.permission.RECORD_AUDIO,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        requestPermissions(permissions, PERMISSION_REQUEST_CODE)
+    }
+
+    companion object {
+        const val PERMISSION_REQUEST_CODE = 123
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/languageribbon_front/VoiceFragment.kt
+++ app/src/main/java/com/example/languageribbon_front/VoiceFragment.kt
@@ -5,11 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.languageribbon_front.databinding.FragmentVoiceBinding
 
 class VoiceFragment : Fragment() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: MyAdapter
     private var _binding: FragmentVoiceBinding? = null
     private val binding get() = _binding!!
+    private var position = 0
+
+    companion object {
+        const val STEP_1 = 0
+        const val STEP_2 = 1
+        const val STEP_3 = 2
+        const val FINAL_STEP = 3
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,5 +33,48 @@ class VoiceFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        adapter = MyAdapter(mutableListOf())
+
+        recyclerView = binding.recyclerView
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        binding.button.setOnClickListener {
+            when (position) {
+                STEP_1 -> transitionToStep(STEP_2, "다음")
+                STEP_2 -> transitionToStep(STEP_3, "다음")
+                STEP_3 -> transitionToStep(FINAL_STEP, "회원가입")
+                else -> {}
+            }
+        }
     }
+    fun updateAdapterData(newData: MutableList<MyElement>) {
+        adapter.setList(newData)
+        adapter.notifyDataSetChanged()
+    }
+    private fun transitionToStep(nextPosition: Int, buttonText: String) {
+        when (position) {
+            STEP_1 -> binding.STEP1.visibility = View.GONE
+            STEP_2 -> binding.STEP2.visibility = View.GONE
+            STEP_3 -> binding.STEP3.visibility = View.GONE
+            FINAL_STEP -> binding.FINALSTEP.visibility = View.GONE
+        }
+
+        position = nextPosition
+
+        binding.stepView.done(false)
+        binding.stepView.go(position, true)
+
+        binding.button.text = buttonText
+
+        when (position) {
+            STEP_1 -> binding.STEP1.visibility = View.VISIBLE
+            STEP_2 -> binding.STEP2.visibility = View.VISIBLE
+            STEP_3 -> binding.STEP3.visibility = View.VISIBLE
+            FINAL_STEP -> binding.FINALSTEP.visibility = View.VISIBLE
+        }
+    }
+
+
 }
