c8de2ac3a85811d35156768994365accddb818c8
choijihyeon <gus1043@gmail.com>
2023-12-11 23:06:42 +0900

    feat : 회원가입 연결

--- app/src/main/java/com/example/languageribbon_front/SignupActivity.kt
+++ app/src/main/java/com/example/languageribbon_front/SignupActivity.kt
@@ -1,31 +1,27 @@
 package com.example.languageribbon_front
 
-import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
-import android.database.Cursor
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
 import android.view.View
-import android.widget.ImageView
 import android.widget.RadioButton
 import android.widget.RadioGroup
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.example.languageribbon_front.databinding.ActivitySignupBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.json.JSONObject
-import java.io.*
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.MalformedURLException
 import java.net.URL
+import java.nio.charset.StandardCharsets
+
 
 class SignupActivity : AppCompatActivity() {
 
@@ -64,37 +60,88 @@ class SignupActivity : AppCompatActivity() {
                     var isExistBlank = false
                     var studentNumCorrect = true
                     var emailCorrect = true
+                    var passwordCorrect = false
 
-                    val email = binding.email.text.toString()
-                    val password = binding.password.text.toString()
                     val name = binding.name.text.toString()
+                    val email = binding.email.text.toString()
+                    val emailDomain = binding.emaildomain.selectedItem.toString()
+                    val username = email + "@" + emailDomain
+                    val password = binding.password.text.toString()
                     val passwordCheck = binding.passwordCheck.text.toString()
-                    val genderRadioGroup = findViewById<RadioGroup>(R.id.gender)
-                    val selectedRadioButtonId = genderRadioGroup.checkedRadioButtonId
-
-                    if (selectedRadioButtonId != -1) {
-                        val selectedRadioButton = findViewById<RadioButton>(selectedRadioButtonId)
-                        val selectedGender = selectedRadioButton.text.toString()
-
-                        // Now you can use the selectedGender as needed
-                    } else {
-                        // No RadioButton is selected, show an error or handle it as appropriate
+                    val age = binding.age.selectedItem.toString()
+                    val ageIndex = when (age) {
+                        "10대" -> 1
+                        "20대" -> 2
+                        "30대" -> 3
+                        else -> -1 // 예외 처리: 해당되는 값이 없을 경우 -1을 할당
                     }
 
+                    val genderRadioGroup = findViewById<RadioGroup>(R.id.gender)
+                    val selectedgenderRadioButtonId = genderRadioGroup.checkedRadioButtonId
+                    val selectedgenderRadioButton = findViewById<RadioButton>(selectedgenderRadioButtonId)
+                    val selectedGender = selectedgenderRadioButton.text.toString()
+                    Log.d("선택된 성별", selectedGender);
+                    val sex = if (selectedGender == "남성") 1 else 2
+
+
+                    val jobRadioGroup = findViewById<RadioGroup>(R.id.job)
+                    val selectedjobRadioButtonId = jobRadioGroup.checkedRadioButtonId
+                    val selectedjobRadioButton = findViewById<RadioButton>(selectedjobRadioButtonId)
+                    val selectedjob = selectedjobRadioButton.text.toString()
+                    Log.d("선택된 직업", selectedjob);
+                    val job = if (selectedjob == "학생") 1 else if (selectedjob == "직장인") 2 else 3
+
+                    val levelRadioGroup = findViewById<RadioGroup>(R.id.level)
+                    val selectedlevelRadioButtonId = levelRadioGroup.checkedRadioButtonId
+                    val selectedlevelRadioButton = findViewById<RadioButton>(selectedlevelRadioButtonId)
+                    val selectedlevel = selectedlevelRadioButton.text.toString()
+                    Log.d("선택된 직업", selectedlevel);
+                    val level = if (selectedlevel == "상") 1 else if (selectedlevel == "중") 2 else 3
+
+                    binding.agreeAll.setOnCheckedChangeListener { _, isChecked ->
+                        binding.agree1.isChecked = isChecked
+                        binding.agree2.isChecked = isChecked
+                    }
+
+                    binding.agree1.setOnCheckedChangeListener { _, isChecked ->
+                        if (!isChecked) {
+                            binding.agreeAll.isChecked = false
+                        } else if (isChecked && binding.agree2.isChecked) {
+                            binding.agreeAll.isChecked = true
+                        }
+                    }
+
+                    binding.agree2.setOnCheckedChangeListener { _, isChecked ->
+                        if (!isChecked) {
+                            binding.agreeAll.isChecked = false
+                        } else if (isChecked && binding.agree1.isChecked) {
+                            binding.agreeAll.isChecked = true
+                        }
+                    }
+
+                    val Agree = binding.agreeAll.isChecked
 
                     // 유저가 항목을 다 채우지 않았을 경우
-                    if (email.isEmpty() && password.isEmpty() && name.isEmpty() && passwordCheck.isEmpty()
-                        && genderRadioGroup.checkedRadioButtonId == -1) {
+                    if (name.isEmpty() && email.isEmpty() && password.isEmpty() && name.isEmpty() && passwordCheck.isEmpty()
+                        && genderRadioGroup.checkedRadioButtonId != -1 && jobRadioGroup.checkedRadioButtonId != -1
+                        && levelRadioGroup.checkedRadioButtonId != -1 && age != "연령대 입력") {
                         isExistBlank = true
                     }
 
+                    //이메일 유효성 체크
+                    if (!android.util.Patterns.EMAIL_ADDRESS.matcher(username).matches()) {
+                        emailCorrect = false
+                    }
 
-                    if (!isExistBlank && emailCorrect && studentNumCorrect) {
+                    if (password.length >= 8 && password.contains("[!@#$%^&*(),.?\":{}|<>]".toRegex())) {
+                        passwordCorrect = true
+                    }
+
+                    if (!isExistBlank && emailCorrect && studentNumCorrect && Agree && passwordCorrect) {
                         // 회원가입 성공 토스트 메세지 띄우기
-                        Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
-                        Log.d("bitmapSignup","$email, $name, $password, ")
+                        Log.d("Signup","$email, $name, $password, $ageIndex, $job, $sex, $level")
                         GlobalScope.launch(Dispatchers.IO) {
-                            val result = performSignup(email, name, password)
+                            val result = performSignup(username, name, sex, ageIndex, level, job, password, passwordCheck)
                             if (result != null) {
                                 runOnUiThread {
                                     //로그인 성공 시 메인 화면으로 이동
@@ -114,29 +161,37 @@ class SignupActivity : AppCompatActivity() {
                         else if(!emailCorrect){ // 입력한 비밀번호가 다를 경우
                             dialog("emailCorrect")
                         }
-//                        else if(!isAgree){ // 이용약관 동의 안한 경우
-//                            dialog("Agree")
-//                        }
+                        else if(!passwordCorrect){ // 입력한 비밀번호가 다를 경우
+                            dialog("passwordCorrect")
+                        }
+                        else if(!Agree){ // 이용약관 동의 안한 경우
+                            dialog("Agree")
+                        }
                     }
                 }
             }
         }
     }
 
-    private suspend fun performSignup(email: String, name: String, password: String): String? {
+    private suspend fun performSignup(email: String, name: String,
+                                      sex: Int, age: Int, job : Int, englishLevel : Int, password1:String, password2:String): String? {
         try {
-            val url = URL("http://jsp.mjdonor.kro.kr:8888/webapp/Android/performSignup.jsp")
+            Log.d("TestRegisterActivity", "Inside performLogin - Start")
+
+            val url = URL("http://ec2-3-12-247-228.us-east-2.compute.amazonaws.com:8000/signup/")
             val conn = url.openConnection() as HttpURLConnection
-            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
             conn.requestMethod = "POST"
+            conn.doOutput = true  // Enable output for the POST request
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded")
 
-            val osw: OutputStream = conn.outputStream
-            val writer = BufferedWriter(OutputStreamWriter(osw, "UTF-8"))
+            val params = "username=$email&name=$name&sex=$sex&age=$age&job=$job&englishLevel=$englishLevel&password1=$password1&password2=$password2"
+            val postData = params.toByteArray(StandardCharsets.UTF_8)
+            Log.d("TestRegisterActivity", "POST 데이터: $params")
+            conn.outputStream.use { os ->
+                os.write(postData)
+            }
 
-            val sendMsg = "email=$email&name=$name&password=$password"
-            Log.d("bitmapPerform", "email=$email&name=$name&password=$password")
-            writer.write(sendMsg)
-            writer.flush()
+            Log.d("Signup", "Data sent to server")
 
             if (conn.responseCode == HttpURLConnection.HTTP_OK) {
                 val tmp = InputStreamReader(conn.inputStream, "UTF-8")
@@ -150,21 +205,33 @@ class SignupActivity : AppCompatActivity() {
                     }
                 }
                 val receiveMsg = buffer.toString()
+
+                Log.d("Login", "Data received from server: $receiveMsg")
+
+                // Parse the JSON data
+                val jsonData = JSONObject(receiveMsg)
+
+                val message = jsonData.optString("message", "")
+
+                Log.e("Signup","${message}")
+
                 return receiveMsg
             } else {
-                Log.d("TestRegisterActivity", "HTTP connection failed with response code: ${conn.responseCode}")
+                Log.d("login", "HTTP connection failed with response code: ${conn.responseCode}")
             }
         } catch (e: MalformedURLException) {
             e.printStackTrace()
+            Log.e("Signup", "MalformedURLException: ${e.message}")
         } catch (e: IOException) {
             e.printStackTrace()
-            Log.e("TestRegisterActivity", "IOException: ${e.message}")
-            Log.e("TestRegisterActivity", "IOException: $e")
+            Log.e("Signup", "IOException: ${e.message}")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.e("Signup", "Exception: ${e.message}")
         }
 
         return null
     }
-
 
     override fun onBackPressed() {
         when (position) {
@@ -209,6 +276,10 @@ class SignupActivity : AppCompatActivity() {
         else if(type.equals("emailCorrect")){
             dialog.setTitle("회원가입 실패")
             dialog.setMessage("이메일 형식을 맞춰주세요")
+        }
+        else if(type.equals("passwordCorrect")){
+            dialog.setTitle("회원가입 실패")
+            dialog.setMessage("비밀번호 형식을 맞춰주세요")
         }
         else if(type.equals("Agree")){
             dialog.setTitle("회원가입 실패")
